260f17ec2fd92fc4f310428a52cc97a9332ed1d6
Yoonsuhan <colle123@naver.com>
2022-06-03 17:32:17 +0900

    220603 네이버뉴스 검색

---
+++ PortFolio/WPFPortFolio/WPFNaverNewsSearch/MainWindow.cs
@@ -0,0 +1,89 @@
+﻿using MahApps.Metro.Controls;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFNaverNewsSearch.Helpers;
+
+namespace WPFNaverNewsSearch
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                //Commons.ShowMessageAsync("실행", "뉴스검색 실행!");
+                SearchNaverNews();
+            }
+        }
+
+        private void SearchNaverNews()
+        {
+            string keyword = txtSearch.Text;
+            string clientID = "_Qjhnv3RpqvZSN06Qd3B";
+            string clientSecret = "WzLF7rg4NU";
+            string base_url = $"https://openapi.naver.com/v1/search/news.json?start=1&display=100&query={keyword}";
+            string result;
+
+            WebRequest request = null;
+            WebResponse response = null;
+            Stream stream = null;
+            StreamReader reader = null;
+
+            // Naver OpenAPI 실제 요청
+            try
+            {
+                request = WebRequest.Create(base_url);
+                request.Headers.Add("X-Naver-Client-Id", clientID); // 중요!
+                request.Headers.Add("X-Naver-Client-Secret", clientSecret); // 중요!!
+
+                response = request.GetResponse();
+                stream = response.GetResponseStream();
+                reader = new StreamReader(stream);
+
+                result = reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                reader.Close();
+                stream.Close();
+                response.Close();
+            }
+
+            MessageBox.Show(result);
+
+            var parseJson = JObject.Parse(result); // string to json
+
+            int total = Convert.ToInt32(parseJson["total"]); // 전체 검색결과 수
+            int display = Convert.ToInt32(parseJson["display"]);
+
+            stsResult.Content = $"{total}개 중 {display}개 호출 성공!";
+        }
+    }
+}
