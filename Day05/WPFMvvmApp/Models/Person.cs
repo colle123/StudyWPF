581f93974970dac8ba7ea9efcbb0fdccd22314fd
Yoonsuhan <colle123@naver.com>
2022-06-07 12:47:42 +0900

    220607 MVVM 오전수업

---
+++ Day05/WPFMvvmApp/Models/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFMvvmApp.Helpers;
+
+namespace WPFMvvmApp.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private string email; // 멤버 변수
+
+        public string Email
+        { // 멤버 속성
+            get { return email; } // get => email; 과 동일
+            set
+            {
+                if (Commons.IsValidEmail(email))
+                    throw new Exception("Invalid Email");
+                else
+                    email = value;
+            }
+        }
+        public DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                var result = Commons.CalcAge(value);
+                if (result > 135 || result < 0)
+                    throw new Exception("Invalid Date");
+                else
+                    date = value;
+            }
+        }
+        public bool IsBirthday
+        {
+            get
+            {
+                return DateTime.Now.Month == Date.Month
+                        && DateTime.Now.Day == Date.Day;
+            }
+        }
+        public bool ISAdult
+        {
+            get
+            {
+                return Commons.CalcAge(date) > 18;
+            }
+        }
+
+        public Person(string firstName, string lastName, string email, DateTime date)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Date = date;
+        }
+    }
+}
---
+++ Day05/WPFMvvmApp/ViewModels/RelayCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFMvvmApp.ViewModels
+{
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> execute; // 실행처리를 위한 Generic
+        private readonly Predicate<T> canExcute; // 실행여부를 판단하는 Generic
+
+        // 이벤트 핸들러의 실행여부에 따라서 이벤트를 추가해주거나 삭제하는 이벤트 핸들러
+        public event EventHandler CanExecuteChanged 
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter) // 명령을 실행 할 수 있는지? -> CanExecute가 활성화 되야만 실행되는 프로그램을 만들게
+        {
+            return canExcute?.Invoke((T)parameter) ?? true; // 입력으로 들어온 parameter을 Generic으로 바꿔서 canExcute를 실행, 실행되면 true를 반환
+        }
+
+        public void Execute(object parameter) // 명령이 있는지?
+        {
+            execute((T)parameter); // 입력으로 들어온 parameter을 Generic으로 바꿔서 execute를 싱행
+        }
+
+        /// <summary>
+        /// execute만 parameter로 받는 생성자
+        /// </summary>
+        /// <param name="execute"></param>
+        /// 
+        public RelayCommand(Action<T> execute) : this(execute, null) { }
+
+        /// <summary>
+        /// execute, canExcute를 parameter로 받는 생성자
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExcute"></param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExcute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExcute = canExcute;
+        }
+    }
+}
