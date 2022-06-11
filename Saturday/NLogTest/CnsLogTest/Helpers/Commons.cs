eb87783f65f2e21dce52cf526c6d80128baf549d
Yoonsuhan <colle123@naver.com>
2022-06-11 15:47:25 +0900

    220611 NLog 학습

---
+++ Saturday/NLogTest/CnsLogTest/Helpers/Commons.cs
@@ -0,0 +1,15 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnsLogTest
+{
+    public class Commons
+    {
+        //NLog용 정적(Static) 인스턴스 생성
+        public static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+    }
+}
